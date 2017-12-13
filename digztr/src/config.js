baa8fa967768bbf9886b1a544c3dc682b0f25ef4
lmajam <majamlemuel@gmail.com>
2017-12-13 13:27:18 +0800

    added app env for production build

--- digztr/src/config.js
+++ digztr/src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   api: {
-    baseUrl: process.env.NODE_ENV === 'production'?'http://ec2-18-217-60-167.us-east-2.compute.amazonaws.com':'http://localhost:8000'
+    baseUrl: process.env.REACT_APP_API_URL || 'http://localhost:8000'
   }
 }
